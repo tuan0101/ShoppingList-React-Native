75a369aa482d449b6f833ad70bfd8dda2b577a4d
Tuan Pham <tuan.programmer0101@gmail.com>
2021-02-20 10:07:36 -0500

    Add uuidv4()

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import { View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './components/Header';
 
 function App() {
+  function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = (c === 'x') ? r : ((r & 0x3) | 0x8);
+      return v.toString(16);
+    });
+  }
+  const [items, setItems] = useState([
+    { id: uuidv4(), text: 'Milk' },
+    { id: uuidv4(), text: 'Eggs' },
+    { id: uuidv4(), text: 'Bread' },
+    { id: uuidv4(), text: 'Juice' },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Header title='Shopping List'/>
+      <Header title='Shopping List' />
+      <FlatList data={items} renderItem={({ item }) => (
+        <Text>{item.text}</Text>
+      )} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
   }
 })
 
